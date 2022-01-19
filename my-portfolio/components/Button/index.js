8f6f21a0f8784144f0718047254c12f4597d3a32
Abhay <abakshi4@my.bcit.ca>
2022-01-19 14:35:05 -0800

    configuring main for development. Branching off.

---
+++ my-portfolio/components/Button/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// styled components imports
+import { ButtonCont } from './styles';
+
+
+const Button = () => {
+  return (
+      <ButtonCont> 
+          Hello There this is the start 
+      </ButtonCont>
+  
+  )
+};
+
+export default Button;
---
+++ my-portfolio/components/Button/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ButtonCont = styled.div`
+
+`
