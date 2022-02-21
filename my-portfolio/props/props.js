4e06ba7a0d76380011b133539c71810933c0c304
Abhay <abakshi4@my.bcit.ca>
2022-02-20 16:54:48 -0800

    changing the info prop

--- my-portfolio/props/props.js
+++ my-portfolio/props/props.js
@@ -6,11 +6,11 @@ export const data = [
     {title:'CSS', width:"100%"},
 ]
 
-export const info={
-    buzzyBee:`Buzzy Bee is a native organizer application that fetches data from the school’s portal (mocked) and automatically adds the student’s course schedule to the built in calendar.
-    The user can:
-    create groups and the app will compare members’ schedules and provide the user with times when everyone is available.create tasks ( Personal, Academic, Group) and add them to the built-in calendar. `
-}
+// export const info={
+//     buzzyBee:`Buzzy Bee is a native organizer application that fetches data from the school’s portal (mocked) and automatically adds the student’s course schedule to the built in calendar.
+//     The user can:
+//     create groups and the app will compare members’ schedules and provide the user with times when everyone is available.create tasks ( Personal, Academic, Group) and add them to the built-in calendar. `
+// }
 
 export const buzzDesign =[
 "/caseStudies/buzzy/buzzLow.png",
