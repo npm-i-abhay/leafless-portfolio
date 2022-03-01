35c4575bad55fbe247a2eef27a387c0ad98a11a1
Abhay <abakshi4@my.bcit.ca>
2022-03-01 09:37:00 -0800

    adding routing for home logo

--- my-portfolio/components/Logo/index.js
+++ my-portfolio/components/Logo/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Router, useRouter } from 'next/router';
 
 import { TechStack } from './styles';
 
 export const Logo = () => {
-  return <div style={{padding:20
+
+const router = useRouter()
+  return <div onClick={()=>{router.push('/')}} style={{padding:20
   }}> 
     <TechStack src='/design/abLogo.svg' /> </div>;
 };
